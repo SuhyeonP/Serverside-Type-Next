3e662a6f1c54bff87688ec24dfb088195dc96bfd
ahah12k <ahah12k@gmail.com>
2020-11-29 01:59:10 +0900

    port

--- back/app.js
+++ back/app.js
@@ -66,10 +66,10 @@ app.use('/user',userRouter);
 app.use('/shop',shopRouter);
 app.use('/shops',shopsRouter);
 app.use('/order',orderRouter);
-
-app.listen(3050, () => {
-  console.log('서버 실행 중!');
-});
-// app.listen(80, () => {
+//
+// app.listen(3050, () => {
 //   console.log('서버 실행 중!');
 // });
+app.listen(80, () => {
+  console.log('서버 실행 중!');
+});
